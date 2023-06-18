4680060f99c3adec45e045c3fff8dce2ecb5e76b
Grim Reaper <htetoowaiyan002@gmail.com>
2023-06-18 11:43:47 -1200

    Grasshopper - Function syntax debugging(8 kuy)

---
+++ code_war_8kuys/grasshopper-function_syntax_debugging.js
@@ -0,0 +1,13 @@
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+let verb1 = 'take ';
+let noun1 = 'time'; 
+
+
+function main(verb , noun){
+    let sentence = verb + noun;
+    return sentence;
+}
+
+console.log(main(verb1,noun1));
