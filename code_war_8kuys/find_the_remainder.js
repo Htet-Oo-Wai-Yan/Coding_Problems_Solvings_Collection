266a109e63fe86971b7354460872f0e01a8c2ad6
Grim Reaper <htetoowaiyan002@gmail.com>
2023-06-24 21:47:19 -1200

    Find the Remainder(8 kyu)

---
+++ code_war_8kuys/find_the_remainder.js
@@ -0,0 +1,33 @@
+// Task:
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+// Examples:
+// n = 17
+// m = 5
+// result = 2 (remainder of `17 / 5`)
+
+// n = 13
+// m = 72
+// result = 7 (remainder of `72 / 13`)
+
+// n = 0
+// m = -1
+// result = 0 (remainder of `0 / -1`)
+
+// n = 0
+// m = 1
+// result - division by zero (refer to the specifications on how to handle this in your language)
+
+let num1 = 13;
+let num2 = 5;
+
+function remainder(n, m){
+    let num = [n,m].sort((a,b) => a - b);
+    let [small,big] = num;
+    return big % small;
+  
+}
+
+console.log(remainder(num1,num2));
